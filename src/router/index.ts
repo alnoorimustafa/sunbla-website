06d6dcf723192fc68f0582b3ab6e065726245236
alnoorimustafa <alnoorimustafa@gmail.com>
2023-08-29 00:57:01 +0300

    last today

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AppVue from '@/App.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/sunbla-website/'),
   routes: [
     {
       path: '/',
